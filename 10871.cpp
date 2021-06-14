41fb03806e84564e3fb379cadd9df0f460571f9c
kass1693 <85857372+kass1693@users.noreply.github.com>
2021-06-14 12:53:26 +0900

    Create 10871.cpp

---
+++ 10871.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int N, X;
+	int a;
+	cin >> N >> X;
+
+	for (int i = 0; i < N; i++) {
+		cin >> a;
+		if (a < X)
+			printf("%d ",a);
+	}
+
+}
