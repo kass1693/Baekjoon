ca2279c46f1bf6a1540adc0b6183bddc7d7b0791
kass1693 <85857372+kass1693@users.noreply.github.com>
2021-06-14 12:52:50 +0900

    Create 10869.cpp

---
+++ 10869.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	cout << n + m << endl;
+	cout << n - m << endl;
+	cout << n * m << endl;
+	cout << n / m << endl;
+	cout << n % m << endl;
+}
