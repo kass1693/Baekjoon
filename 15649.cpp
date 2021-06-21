17dcdf851f1baec8f44635f8eb8ac9e54c45e231
kass1693 <85857372+kass1693@users.noreply.github.com>
2021-06-21 21:08:52 +0900

    Create 15649.cpp

---
+++ 15649.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string.h>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#include <map>
+
+using namespace std;
+bool ar[10];
+vector<int> v;
+int n, m;
+void dfs(int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << v[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (ar[i]) continue;
+		ar[i] = true;
+		v.push_back(i);
+		dfs(cnt + 1);
+		ar[i] = false;
+		v.pop_back();
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+
+	dfs(0);
+}
+
